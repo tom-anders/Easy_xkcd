87b17aa2b4ce625628932399bb77ba1efd221569
Peter Hindes <ph9214@gmail.com>
2020-01-19 14:59:18 -0700

    max lines in alt text

--- app/src/androidTest/java/de/tap/easy_xkcd/ApplicationTest.java
+++ app/src/androidTest/java/de/tap/easy_xkcd/ApplicationTest.java
@@ -5,9 +5,9 @@ import android.test.ApplicationTestCase;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
- */
+ *
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
-}
+} */
