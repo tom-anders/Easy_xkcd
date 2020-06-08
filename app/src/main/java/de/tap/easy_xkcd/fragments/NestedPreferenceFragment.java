e4edd944ffd36b887fc52fba6fb407697b87d698
mueller-ma <mueller-ma@users.noreply.github.com>
2020-06-08 19:48:53 +0200

    Fix enabled state of preference

--- app/src/main/java/de/tap/easy_xkcd/fragments/NestedPreferenceFragment.java
+++ app/src/main/java/de/tap/easy_xkcd/fragments/NestedPreferenceFragment.java
@@ -352,6 +352,7 @@ public class NestedPreferenceFragment extends PreferenceFragment {
                         }
                     }
                 });
+                findPreference(MOBILE_ENABLED).setEnabled(MainActivity.fullOffline | MainActivity.fullOfflineWhatIf);
                 break;
 
 
